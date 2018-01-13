0824d04cc3b10970d1932fca654667a922c1557f
marten <marten@ridango.com>
2018-01-13 19:22:46 +0200

    changed the dest for screenshots

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,5 +1,10 @@
 package example;		
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -25,6 +30,14 @@ public class NewTest {
 			}
 			String title = driver.getTitle();	
 			System.out.println(title);
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			// Now you can do whatever you need to do with it, for example copy somewhere
+			try {
+				FileUtils.copyFile(scrFile, new File("target\\sh\\screenshot.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			Assert.assertTrue(title.contains("Tallinn")); 		
 		}	
 		@BeforeTest
